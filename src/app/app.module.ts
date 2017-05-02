17f3d04cabdc03e8a1e29ab9bb6763a479f33745
wangdong <mail@wangdong.io>
2017-05-02 06:25:31 +0000

    优化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 import {SharedModule} from './shared/shared.module';
 import {AppRoutingModule} from './app-routing.module';
@@ -13,7 +13,7 @@ import {AppComponent} from './app.component';
     ],
     imports: [
         BrowserModule,
-        NoopAnimationsModule,
+        BrowserAnimationsModule,
         SharedModule,
         AppRoutingModule,
         AppParseModule,
