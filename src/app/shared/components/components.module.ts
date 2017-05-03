23eb54ad45003ca103b3681d6fa665ed9b99bcf4
wangdong <mail@wangdong.io>
2017-05-03 09:06:09 +0000

    增加font-awesome字体

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {FooterModule} from './footer/footer.module';
+import {HeaderModule} from './header/header.module';
 
 @NgModule({
     exports: [
         FooterModule,
+        HeaderModule,
     ]
 })
 export class ComponentsModule {
--- src/app/shared/components/header/header.module.ts
+++ src/app/shared/components/header/header.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 
 import {SharedModuleModule} from '../../shared-module.module';
-import {FooterComponent} from './footer.component';
+import {HeaderComponent} from './header.component';
 
 @NgModule({
     imports: [
         SharedModuleModule
     ],
-    declarations: [FooterComponent],
-    exports: [FooterComponent]
+    declarations: [HeaderComponent],
+    exports: [HeaderComponent]
 })
-export class FooterModule {
+export class HeaderModule {
 }
