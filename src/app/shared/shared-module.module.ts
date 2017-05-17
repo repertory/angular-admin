7420af033262c46a74edd4d180f233fcde8c2906
wangdong <mail@wangdong.io>
2017-05-17 09:55:19 +0000

    升级扩展

--- src/app/shared/shared-module.module.ts
+++ src/app/shared/shared-module.module.ts
@@ -4,7 +4,15 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {RouterModule} from '@angular/router';
 
-import {CovalentCoreModule} from '@covalent/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MaterialModule} from '@angular/material';
+import {
+    CovalentDataTableModule,
+    CovalentDialogsModule,
+    CovalentNotificationsModule,
+    CovalentPagingModule,
+    CovalentSearchModule
+} from '@covalent/core';
 
 @NgModule({
     imports: [
@@ -16,7 +24,13 @@ import {CovalentCoreModule} from '@covalent/core';
         FormsModule,
         RouterModule,
         HttpModule,
-        CovalentCoreModule,
+        FlexLayoutModule,
+        MaterialModule,
+        CovalentDataTableModule,
+        CovalentDialogsModule,
+        CovalentNotificationsModule,
+        CovalentPagingModule,
+        CovalentSearchModule
     ]
 })
 export class SharedModuleModule {
