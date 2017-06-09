2a20e52a0a79e52066bb2d704bad447a0c6722f6
wangdong <mail@wangdong.io>
2017-06-09 15:57:39 +0800

    增加图片预览功能

--- src/app/shared/directives/gallery/gallery.directive.ts
+++ src/app/shared/directives/gallery/gallery.directive.ts
@@ -10,10 +10,14 @@ export class GalleryDirective {
 
     @HostListener('mouseenter') onMouseEnter() {
         window['lightGallery'](this.element.nativeElement, {
-            subHtmlSelectorRelative: true,
-            selector: 'img',
+            selector: '[appGalleryItem]',
             cssEasing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
         });
     }
+}
 
+@Directive({
+    selector: '[appGalleryItem]'
+})
+export class GalleryItemDirective {
 }
