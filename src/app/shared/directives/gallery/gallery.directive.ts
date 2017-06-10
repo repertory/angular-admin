dad08d07b0376f99c6148d90a8617c8e81ea1000
wangdong <mail@wangdong.io>
2017-06-10 12:08:27 +0800

    优化

--- src/app/shared/directives/gallery/gallery.directive.ts
+++ src/app/shared/directives/gallery/gallery.directive.ts
@@ -5,14 +5,14 @@ import {Directive, ElementRef, HostListener} from '@angular/core';
 })
 export class GalleryDirective {
 
-    constructor(private element: ElementRef) {
-    }
-
     @HostListener('mouseenter') onMouseEnter() {
         window['lightGallery'](this.element.nativeElement, {
             selector: '[appGalleryItem]',
             cssEasing: 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
         });
+    }
+
+    constructor(private element: ElementRef) {
     }
 }
 
