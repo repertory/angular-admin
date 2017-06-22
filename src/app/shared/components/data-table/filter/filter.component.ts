0ba6807f3dcd7bc16da1b31889b7b1c20e63eea8
wangdong <mail@wangdong.io>
2017-06-22 15:29:07 +0800

    优化

--- src/app/shared/components/data-table/filter/filter.component.ts
+++ src/app/shared/components/data-table/filter/filter.component.ts
@@ -11,9 +11,61 @@ import {DataTableOption} from '../data-table';
 export class FilterComponent {
 
     options: DataTableOption[];
+    filters: any[] = [];
+    where = {
+        'String': [
+            {
+                name: '存在',
+                key: 'exists',
+            },
+            {
+                name: '不存在',
+                key: 'doesNotExist',
+            },
+            {
+                name: '等于',
+                key: 'equalTo',
+            },
+            {
+                name: '不等于',
+                key: 'notEqualTo',
+            },
+            {
+                name: '起始于',
+                key: 'startsWith',
+            },
+            {
+                name: '结束于',
+                key: 'endsWith',
+            },
+        ]
+    };
 
     constructor(@Inject(MD_DIALOG_DATA) public data: any) {
         this.options = data.options.filter(x => x.operate.query.enabled);
+
+        if (!this.filters.length) {
+            this.add();
+        }
     }
 
+    add() {
+        this.filters.push({});
+    }
+
+    remove(index) {
+        if (this.filters.length < 2) {
+            return false;
+        }
+        this.filters.splice(index, 1);
+    }
+
+    getWhere(key): any[] {
+        const option = this.options.filter(x => x.key === key).pop();
+        console.log(option);
+        if (!option || !option.type) {
+            return [];
+        }
+        return this.where[option.type] || [];
+    }
 }
