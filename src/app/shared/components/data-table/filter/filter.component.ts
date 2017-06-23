6c08c6a12b616ea7a91eb3ebece2676a06a2edf4
wangdong <mail@wangdong.io>
2017-06-23 14:06:09 +0800

    优化

--- src/app/shared/components/data-table/filter/filter.component.ts
+++ src/app/shared/components/data-table/filter/filter.component.ts
@@ -12,8 +12,8 @@ export class FilterComponent {
 
     options: DataTableOption[];
     filters: any[] = [{}];
-    where = {
-        'String': [
+    type = {
+        'string': [
             {
                 name: '存在',
                 key: 'exists',
@@ -38,7 +38,27 @@ export class FilterComponent {
                 name: '结束于',
                 key: 'endsWith',
             },
-        ]
+        ],
+        'number': [
+            {
+                name: '存在',
+                key: 'exists',
+            },
+            {
+                name: '不存在',
+                key: 'doesNotExist',
+            },
+        ],
+        'default': [
+            {
+                name: '存在',
+                key: 'exists',
+            },
+            {
+                name: '不存在',
+                key: 'doesNotExist',
+            },
+        ],
     };
 
     constructor(@Inject(MD_DIALOG_DATA) public data: any) {
@@ -56,12 +76,28 @@ export class FilterComponent {
         this.filters.splice(index, 1);
     }
 
-    getWhere(key): any[] {
+    getType(key): any[] {
         const option = this.options.filter(x => x.key === key).pop();
-        console.log(option);
         if (!option || !option.type) {
             return [];
         }
-        return this.where[option.type] || [];
+        return this.type[option.type.toLowerCase()] || this.type['default'];
+    }
+
+    getValueType(filter): string {
+        const option = this.options.filter(x => x.key === filter.key).pop();
+        if (!option || !option.type) {
+            return '';
+        }
+
+        switch (filter.type) {
+            case 'equalTo':
+            case 'notEqualTo':
+            case 'startsWith':
+            case 'endsWith':
+                return option.type.toLowerCase();
+        }
+
+        return '';
     }
 }
